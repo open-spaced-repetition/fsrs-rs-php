21a999d2dc26dce1fb495e865a893f4f2ca684e1
Asuka Minato <i@asukaminato.eu.org>
2025-05-20 11:52:42 +0900

    Update dev.nix
    close https://github.com/davidcole1340/ext-php-rs/issues/409

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,22 +6,45 @@
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
     pkgs.rustup
     pkgs.clang-tools
     pkgs.clang
     pkgs.libclang
-    pkgs.php81.unwrapped.dev # https://github.com/NixOS/nixpkgs/issues/110826
+    pkgs.php81.unwrapped # Provides the PHP executable
+    pkgs.php81.unwrapped.dev
+    pkgs.buildPackages.stdenv.cc.cc.lib # Provides standard C headers like stdlib.h
+    pkgs.pkg-config # often useful for build scripts
+    pkgs.fish
+    pkgs.git
   ];
 
   # Sets environment variables in the workspace
   env = {
-   
+    PATH = pkgs.lib.mkForce "${pkgs.php81.unwrapped}/bin:${pkgs.php81.unwrapped.dev}/bin:${pkgs.clang}/bin:$PATH";
+    PHP_CONFIG = "${pkgs.php81.unwrapped.dev}/bin/php-config";
+    PHP = "${pkgs.php81.unwrapped}/bin/php";
+    LIBCLANG_PATH = pkgs.lib.mkForce "${pkgs.libclang.lib}/lib";
+
+    # Explicitly add include paths for bindgen and cc crate
+    # This tells clang (used by bindgen) and the cc crate where to find system headers.
+    BINDGEN_EXTRA_CLANG_ARGS = pkgs.lib.mkForce (
+      # Includes from the C standard library (glibc)
+      "-I${pkgs.glibc.dev}/include " +
+      # Includes from the selected PHP version
+      "-I${pkgs.php81.unwrapped.dev}/include/php " +
+      "-I${pkgs.php81.unwrapped.dev}/include/php/main " +
+      "-I${pkgs.php81.unwrapped.dev}/include/php/TSRM " +
+      "-I${pkgs.php81.unwrapped.dev}/include/php/Zend "
+      # You might need to add other specific paths from php-config --includes if they are missing
+    );
+
+    # Setting C_INCLUDE_PATH might also help the `cc` crate directly
+    C_INCLUDE_PATH = pkgs.lib.mkForce "${pkgs.glibc.dev}/include";
+
+    # Ensure clang from nixpkgs is used by cc crate and bindgen
+    CC = "${pkgs.clang}/bin/clang";
   };
+
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
