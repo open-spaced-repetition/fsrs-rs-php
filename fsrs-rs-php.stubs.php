c96473b907d470b386341f8b0809ce524b3af98b
Asuka Minato <i@asukaminato.eu.org>
2025-05-26 03:08:37 +0900

    try (#9)
    * autofix
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- fsrs-rs-php.stubs.php
+++ fsrs-rs-php.stubs.php
@@ -3,78 +3,91 @@
 // Stubs for fsrs-rs-php
 
 namespace fsrs {
-    class FSRS {
-        public function __construct(array $parameters) {}
-        public function next_states(?MemoryState $current_memory_state, float $desired_retention, int $days_elapsed): NextStates {}
-        public function compute_parameters(array $train_set): array {}
-        public function benchmark(array $train_set): array {}
-        public function memory_state_from_sm2(float $ease_factor, float $interval, float $sm2_retention): MemoryState {}
-        public function memory_state(FSRSItem $item, ?MemoryState $starting_state = null): MemoryState {}
-    }
-
-    class MemoryState {
-        public function __construct(float $stability, float $difficulty) {}
-        public function get_stability(): float {}
-        public function get_difficulty(): float {}
-        public function __toString(): string {}
-    }
-
-    class NextStates {
-        public function get_hard(): ItemState {}
-        public function get_good(): ItemState {}
-        public function get_easy(): ItemState {}
-        public function get_again(): ItemState {}
-    }
-
     class ItemState {
-        public function get_memory(): MemoryState {}
+        public function get_memory(): \fsrs\MemoryState {}
+
         public function get_interval(): float {}
-        public function __toString(): string {}
-    }
 
-    class FSRSItem {
-        public function __construct(array $reviews) {}
-        public function get_reviews(): array {}
-        public function set_reviews(array $reviews): void {}
-        public function long_term_review_cnt(): int {}
-        public function __toString(): string {}
-    }
-
-    class FSRSReview {
-        public function __construct(int $rating, int $delta_t) {}
-        public function get_rating(): int {}
-        public function get_delta_t(): int {}
         public function __toString(): string {}
     }
 
     class SimulationResult {
         public function get_memorized_cnt_per_day(): array {}
+
         public function get_review_cnt_per_day(): array {}
+
         public function get_learn_cnt_per_day(): array {}
+
         public function get_cost_per_day(): array {}
+
         public function get_correct_cnt_per_day(): array {}
     }
 
     class SimulatorConfig {
         public function __construct() {}
     }
+
+    class FSRS {
+        public function __construct(array $parameters) {}
+
+        public function next_states(?\fsrs\MemoryState $current_memory_state, float $desired_retention, int $days_elapsed): \fsrs\NextStates {}
+
+        public function compute_parameters(array $train_set): array {}
+
+        public function benchmark(array $train_set): array {}
+
+        public function memory_state_from_sm2(float $ease_factor, float $interval, float $sm2_retention): \fsrs\MemoryState {}
+
+        public function memory_state(\fsrs\FSRSItem $item, ?\fsrs\MemoryState $starting_state): \fsrs\MemoryState {}
+    }
+
+    class FSRSReview {
+        public function __construct(int $rating, int $delta_t) {}
+
+        public function get_rating(): int {}
+
+        public function get_delta_t(): int {}
+
+        public function __toString(): string {}
+    }
+
+    class MemoryState {
+        public function __construct(float $stability, float $difficulty) {}
+
+        public function get_stability(): float {}
+
+        public function get_difficulty(): float {}
+
+        public function __toString(): string {}
+    }
+
+    class FSRSItem {
+        public function __construct(array $reviews) {}
+
+        public function get_reviews(): array {}
+
+        public function set_reviews(array $reviews) {}
+
+        public function long_term_review_cnt(): int {}
+
+        public function __toString(): string {}
+    }
+
+    class NextStates {
+        public function get_hard(): \fsrs\ItemState {}
+
+        public function get_good(): \fsrs\ItemState {}
+
+        public function get_easy(): \fsrs\ItemState {}
+
+        public function get_again(): \fsrs\ItemState {}
+    }
 }
 
-/**
- * @param array $w
- * @param float $desired_retention
- * @param SimulatorConfig|null $config
- * @param int|null $seed
- * @return SimulationResult
- */
-function simulate(array $w, float $desired_retention, ?SimulatorConfig $config = null, ?int $seed = null): SimulationResult {}
+namespace {
+    function simulate(array $w, float $desired_retention, ?\fsrs\SimulatorConfig $config, ?int $seed): \fsrs\SimulationResult {}
 
-/**
- * @return SimulatorConfig
- */
-function default_simulator_config(): SimulatorConfig {}
+    function default_simulator_config(): \fsrs\SimulatorConfig {}
 
-/**
- * @return array
- */
-function get_default_parameters(): array {}
+    function get_default_parameters(): array {}
+}
